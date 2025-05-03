668bda3332d1b5fdaac2a8c3013c209e9bfb960d
준 <wjdwns4552@edu.dju.ac.kr>
2025-05-03 23:24:56 +0900

    🧠 feat: Study LeetCode two-sum [Array]

---
+++ 02_PS/LeetCode/two-sum/leet_two-sum_004.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype
+        """
+        nums_map = {}
+        # 하나의 for문으로 통합        
+        for i, num in enumerate(nums):
+            if target -num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
